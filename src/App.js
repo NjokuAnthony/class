d50b22a295d49d16f681fc0441fe91b565d1f8a4
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-04-14 10:21:09 -0700

    landingPage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Header from './componets/Header'
 import Feature from './componets/Feature'
 import About from './componets/About'
+import Presentation from './componets/Presentation'
 import aboutimage from './images/Frame 19.png'
 import aboutimage1 from './images/download.png'
+import Contact from './componets/Contact'
+
 
 function App() {
   return (
@@ -11,8 +14,10 @@ function App() {
       
       <Header />
       <Feature />
-      <About image={aboutImage} title='Comes with All You Need for Daily Life' button='Get The App' />
-      <About image={aboutImage1} title='Download And Get The App Now' button='Download' />
+      <About image={aboutimage} title='Comes with All You Need for Daily Life' button='Get The App' />
+      <Presentation />
+      <About image={aboutimage1} title='Download And Get The App Now' button='Download' />
+      <Contact />
     </div>
   );
 }
